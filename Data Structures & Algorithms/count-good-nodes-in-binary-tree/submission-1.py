3b833ee6242d5363e3dab378bac53b35d50a918d
Nilavanluna <103325766+Nilavanluna@users.noreply.github.com>
2026-06-16 10:04:06 +0100

    Add: count-good-nodes-in-binary-tree - submission-1

---
+++ Data Structures & Algorithms/count-good-nodes-in-binary-tree/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        self.count = 0
+
+        def dfs(node, max_so_far):
+            if not node:
+                return
+
+            if node.val >= max_so_far:
+                self.count += 1
+
+            max_so_far = max(max_so_far, node.val)
+
+            dfs(node.left, max_so_far)
+            dfs(node.right, max_so_far)
+
+        dfs(root, root.val)
+        return self.count
